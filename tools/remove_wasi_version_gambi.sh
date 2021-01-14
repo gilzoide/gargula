8bcc176d151650ca5c9b7025def1e656442e73d5
gilzoide <gilzoide@gmail.com>
2021-01-14 04:49:31 -0300

    Fix remove_wasi_version_gambi paths

--- tools/remove_wasi_version_gambi.sh
+++ tools/remove_wasi_version_gambi.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 script_root=$(dirname $(realpath $0))
+project_root="$script_root/.."
 
-core_file="$script_root/druntime/src/core/sys/wasi/core.d"
-config_file="$script_root/druntime/src/core/sys/wasi/config.d"
-types_file="$script_root/druntime/src/core/sys/wasi/sys/types.d"
+core_file="$project_root/libs/druntime/src/core/sys/wasi/core.d"
+config_file="$project_root/libs/druntime/src/core/sys/wasi/config.d"
+types_file="$project_root/libs/druntime/src/core/sys/wasi/sys/types.d"
 
 lua_script='print((string.gsub(io.read("*a"), "version[^:]+:", "")))'
 echo "$(cat $core_file | lua -e "$lua_script")" > $core_file
