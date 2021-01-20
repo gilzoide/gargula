a3cce39a18fc255b5d77a556cd226758a555dfe6
gilzoide <gilzoide@gmail.com>
2021-01-19 21:40:17 -0300

    Add "texture loading and drawing" showing Game.Texture Flyweight

---
+++ examples/texture_loading_and_drawing.d
@@ -0,0 +1,44 @@
+import gargula;
+
+enum GameConfig gameConfig = {
+    title: "mouse input",
+    // Texture files declared here are stored in a Flyweight instance
+    // and be reference counted/automatically unloaded when not needed
+    textures: [
+        "oi.png",
+    ],
+};
+alias Game = GameTemplate!(gameConfig);
+
+struct Scene
+{
+    mixin Node;
+
+    // Use `Game.Texture` instead of `Texture` to use the Flyweight
+    Game.Texture texture;
+
+    // This function is called by `game.create` and is used to initialize
+    // state at runtime, as D does not support struct empty constructors
+    void initialize()
+    {
+        // Textures in `gameConfig.textures` are created once when needed
+        // and reused afterwards. Non identifier characters are converted
+        // to underscores: "oi.png" -> "oi_png"
+        texture = Game.Texture.oi_png;
+    }
+
+    void draw()
+    {
+        DrawTexture(texture, Game.config.width / 2 - texture.width / 2, Game.config.height / 2 - texture.height / 2, WHITE);
+
+        DrawText("this IS a texture!", 360, 370, 10, GRAY);
+    }
+}
+
+void main(string[] args)
+{
+    auto game = Game(args);
+    game.create!Scene;
+    game.run();
+}
+
