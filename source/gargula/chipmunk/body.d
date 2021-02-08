c48851dbbbea15ce39a279b9fa89f40b9c658cd4
gilzoide <gilzoide@gmail.com>
2021-02-08 18:09:06 -0300

    Add Body.{draw,lateDraw} setting rlgl MODELVIEW matrix

--- source/gargula/chipmunk/body.d
+++ source/gargula/chipmunk/body.d
@@ -1,11 +1,16 @@
 module gargula.chipmunk.body;
 
+import bettercmath;
 import betterclist;
 import chipmunk;
 
 import gargula.chipmunk.space;
 import gargula.log;
 import gargula.node;
+import gargula.wrapper.rlgl;
+
+private alias Vector2 = Vector!(float, 2);
+private alias Transform3D = Transform!(float, 3);
 
 enum bodyStackSize = 4;
 struct Body
@@ -40,8 +45,21 @@ private mixin template BodyTemplate(string newCall)
         Body.bodyStack.pop();
     }
 
-    ~this()
+    void draw()
     {
+        rlMatrixMode(RL_MODELVIEW);
+        rlPushMatrix();
+        auto t = Transform3D.identity
+            .translate(-cast(Vector2) cpBodyGetCenterOfGravity(body_))
+            .rotate(cast(float) -cpBodyGetAngle(body_))
+            .translate(cast(Vector2) cpBodyGetPosition(body_));
+        rlMultMatrixf(t.matrix.elements.ptr);
+    }
+    void lateDraw()
+    {
+        rlPopMatrix();
+    }
+
     //~this()
     //{
         //if (body_)
